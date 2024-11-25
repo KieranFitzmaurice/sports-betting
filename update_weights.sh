b1c466322cfa6629069231896b71d4326bbb27f2
KieranFitzmaurice <kpf10@pitt.edu>
2024-11-25 08:14:29 -0500

    fixed issue with activation of mamba environment within bash script

--- update_weights.sh
+++ update_weights.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-cd /home/kieran/projects/sports-betting
+cd ~/projects/sports-betting
 source sports-betting-venv-1/bin/activate
 python scrape_schedules.py
 python scrape_scores.py
 python attach_outcomes.py
 deactivate
+source ~/miniforge3/etc/profile.d/conda.sh
+source ~/miniforge3/etc/profile.d/mamba.sh
 mamba activate pymc_env
 export OLD_LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/miniforge3/lib
